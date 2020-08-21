dbb2c0e879b074d6e1513e251a18534fb49e7aff
idk <idk@laptop.idk.i2p>
2020-08-21 17:27:33 -0400

    change module name

--- firefox.go
+++ firefox.go
@@ -5,7 +5,7 @@ Released under the The MIT License (MIT)
 see ./LICENSE
 */
 
-import "github.com/eyedeekay/i2pfirefox/lib"
+import "github.com/eyedeekay/i2pbrowser/lib"
 
 func firefoxMain() {
 	i2pfirefox.FirefoxMain()
