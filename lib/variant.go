d4d2beef0c2198dcb7c646d0ae1517feac3d5f9c
idk <idk@laptop.idk.i2p>
2020-08-21 16:35:54 -0400

    Improve release automation

--- lib/variant.go
+++ lib/variant.go
@@ -108,7 +108,7 @@ user_pref("browser.selfsupport.url",				"");
 // PREF: Disable Firefox Hello (disabled) (Firefox < 49)
 // https://wiki.mozilla.org/Loop
 // https://support.mozilla.org/t5/Chat-and-share/Support-for-Hello-discontinued-in-Firefox-49/ta-p/37946
-// NOTICE-DISABLED: Firefox Hello requires setting `media.peerconnection.enabled` and `media.getusermedia.screensharing.enabled` to true, `security.OCSP.require` to false to work.
+// NOTICE-DISABLED: Firefox Hello requires setting "media.peerconnection.enabled" and "media.getusermedia.screensharing.enabled" to true, "security.OCSP.require" to false to work.
 //user_pref("loop.enabled",		false);
 
 // PREF: Disable Firefox Hello metrics collection
