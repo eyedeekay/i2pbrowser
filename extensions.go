5011c95bcbda8d3641901da5b2d7db301fa4eb9a
idk <idk@laptop.idk.i2p>
2020-09-29 15:06:17 -0400

    add prefs to auto-enable scoped extensions

--- extensions.go
+++ extensions.go
@@ -7,7 +7,7 @@ Released under the The MIT License (MIT)
 see ./LICENSE
 */
 
-var VERSION = "0.77"
+var VERSION = "0.79"
 var SNOW_VERSION = "0.2.2"
 var UMAT_VERSION = "1.25.2"
 var UBLO_VERSION = "1.4.0"
